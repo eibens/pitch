1500b7339ff0ade9fe56e8421e756c2b162c14b7
eibens <l.eibensteiner@gmail.com>
2021-07-22 03:16:35 +0200

    feat(midi): inverse mappings

--- midi.ts
+++ midi.ts
@@ -1,14 +1,19 @@
 import { Accidental, Letter, Pitch } from "./pitch.ts";
+import * as DIATONIC from "./diatonic.ts";
+import {
+  decompose,
+  modulo,
+  quotient,
+} from "https://deno.land/x/modulo@v0.0.9/mod.ts";
 
 /**
  * Converts a pitch letter to a semitone offset within the C major scale.
  *
  * Since MIDI number zero corresponds to C-1 (one octave below C0), C is used as the origin.
  *
- * @param letter is the pitch letter that should be converted.
- * @returns is the corresponding semitone offset.
+ * @param letter is the pitch letter.
+ * @returns the semitone offset.
  */
-
 export function fromLetter(letter: Letter): number {
   return {
     C: 0,
@@ -22,12 +27,29 @@ export function fromLetter(letter: Letter): number {
 }
 
 /**
+ * Inverse function of `fromLetter`.
+ *
+ * @param midi is the semitone offset.
+ * @param accidental is the preferred accidental.
+ * @returns the pitch letter.
+ */
+export function toLetter(midi: number, accidental: "#" | "b"): Letter {
+  const diatonic = DIATONIC.fromSemitones(midi);
+  const [round, rest] = decompose(diatonic, 1);
+  const halfStep = rest === 0.5;
+  const increment = accidental === "b" && halfStep;
+  const whole = round + (increment ? 1 : 0);
+  const index = modulo(whole, 7);
+  return [..."CDEFGAB"][index] as Letter;
+}
+
+/**
  * Converts an accidental to a semitone offset.
  *
  * This works because [enharmonic](https://en.wikipedia.org/wiki/Enharmonic) notes are mapped to the same MIDI number.
  *
- * @param accidental is the accidental that should be converted.
- * @returns is the corresponding semitone offset.
+ * @param accidental is the accidental.
+ * @returns the semitone offset.
  */
 export function fromAccidental(accidental: Accidental): number {
   return {
@@ -40,10 +62,24 @@ export function fromAccidental(accidental: Accidental): number {
 }
 
 /**
+ * Inverse function of `fromAccidental`.
+ *
+ * @param midi is the semitone offset.
+ * @param accidental is the preferred accidental.
+ * @returns the accidental.
+ */
+export function toAccidental(midi: number, accidental: "#" | "b"): Accidental {
+  const diatonic = DIATONIC.fromSemitones(midi);
+  const partial = modulo(diatonic, 1);
+  if (partial !== 0.5) return "";
+  return accidental;
+}
+
+/**
  * Convert an octave number in SPN to semitone offset from C-1.
  *
- * @param octave is the octave number that should be converted.
- * @returns is the corresponding semitone offset.
+ * @param octave is the octave number.
+ * @returns is the semitone offset.
  */
 export function fromOctave(octave: number) {
   // NOTE: There are 12 semitones per octave. Since MIDI pitch zero corresponds to C-1 (one octave below C0), we add 1 as an additional offset.
@@ -51,14 +87,39 @@ export function fromOctave(octave: number) {
 }
 
 /**
+ * Inverse function of `fromOctave`.
+ *
+ * @param midi is the semitone offset.
+ * @returns the octave number.
+ */
+export function toOctave(midi: number): number {
+  return quotient(midi, 12) - 1;
+}
+
+/**
  * Converts a pitch to a MIDI pitch number.
  *
- * @param pitch is the pitch data that should be converted.
- * @returns is the corresponding MIDI pitch.
+ * @param pitch is the pitch data.
+ * @returns the MIDI pitch.
  */
 export function fromPitch(pitch: Pitch) {
   const { letter, octave, accidental } = pitch;
   return fromLetter(letter) +
     fromAccidental(accidental) +
     fromOctave(octave);
+}
+
+/**
+ * Inverse function of `fromPitch`.
+ *
+ * @param midi is the semitone offset.
+ * @param accidental is the preferred accidental.
+ * @returns the pitch data.
+ */
+export function toPitch(midi: number, accidental: "#" | "b"): Pitch {
+  return {
+    letter: toLetter(midi, accidental),
+    accidental: toAccidental(midi, accidental),
+    octave: toOctave(midi),
+  };
 }
--- midi_test.ts
+++ midi_test.ts
@@ -1,23 +1,69 @@
 import { assertEquals } from "https://deno.land/std@0.90.0/testing/asserts.ts";
-import { fromAccidental, fromLetter, fromOctave, fromPitch } from "./midi.ts";
+import {
+  fromAccidental,
+  fromLetter,
+  fromOctave,
+  fromPitch,
+  toAccidental,
+  toLetter,
+  toOctave,
+  toPitch,
+} from "./midi.ts";
 
 Deno.test("fromLetter maps A -> 9", () => {
   assertEquals(fromLetter("A"), 9);
 });
 
-Deno.test("fromAccidental maps A -> 9", () => {
+Deno.test("toLetter maps 9 b -> A", () => {
+  assertEquals(toLetter(9, "b"), "A");
+});
+
+Deno.test("toLetter maps 10 # -> A#", () => {
+  assertEquals(toLetter(10, "#"), "A");
+});
+
+Deno.test("toLetter maps 10 b -> A", () => {
+  assertEquals(toLetter(10, "b"), "B");
+});
+
+Deno.test("fromAccidental maps bb -> -2", () => {
   assertEquals(fromAccidental("bb"), -2);
+});
+
+Deno.test("toAccidental maps 60 b -> ''", () => {
+  assertEquals(toAccidental(60, "b"), "");
+});
+
+Deno.test("toAccidental maps 70 # -> #", () => {
+  assertEquals(toAccidental(70, "#"), "#");
+});
+
+Deno.test("toAccidental maps 70 b -> b", () => {
+  assertEquals(toAccidental(70, "b"), "b");
 });
 
 Deno.test("fromOctave 4 -> 60", () => {
   assertEquals(fromOctave(4), 60);
 });
 
-Deno.test("fromPitch maps A#4 -> 60", () => {
+Deno.test("toOctave 60 -> 4", () => {
+  assertEquals(toOctave(60), 4);
+});
+
+Deno.test("fromPitch maps A#4 -> 70", () => {
   const res = fromPitch({
     letter: "A",
     accidental: "#",
     octave: 4,
   });
   assertEquals(res, 70);
+});
+
+Deno.test("fromPitch maps 70 -> A#4", () => {
+  const res = toPitch(70, "#");
+  assertEquals(res, {
+    letter: "A",
+    accidental: "#",
+    octave: 4,
+  });
 });
